c9910af05b4a4d204ff888135f3573269bc90308
Joshua Ye <joshua.ye2005@gmail.com>
2025-03-17 13:47:31 -0500

    checkpoint frontend

--- backend/index.js
+++ backend/index.js
@@ -2,24 +2,25 @@ import express, { request } from "express";
 import {PORT, mongoDBURL} from "./config.js";
 import mongoose, { mongo } from "mongoose";
 import Route from "./routes/bookRoutes.js";
+import cors from "cors";
 
 const app = express()
 
 app.use(express.json());
 
 
-app.use(cors({
-    origin: "http://localhost:5555",
-    methods: ['GET','POST','PUT','DELETE'],
-    allowedHeaders: ['Content-Type'],
-}));
+app.use(cors());
+
+// app.use(cors({
+//     origin: "http://localhost:5555",
+//     methods: ['GET','POST','PUT','DELETE'],
+//     allowedHeaders: ['Content-Type'],
+// }));
 
 app.get('/', (req, res) => {
     console.log(req);
     return res.status(234).send("hi");
 }) //endpoint, callback function (handler)
-
-app.listen;
 
 app.use('/books', Route);
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import {Route, Routers} from 'react-router-dom';
+import EditBook from './pages/EditBooks';
+import CreateBook from './pages/CreateBooks';
+import DeleteBook from './pages/DeleteBooks';
+import Home from './pages/Home';
+import ShowBook from './pages/ShowBook';
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'>
-      Hello
-    </div>
-  )
-}
+    <Routers>
+      <Route path='/' element={<Home />} />
+      <Route path='/books/create' element={<CreateBook />} />
+      <Route path='/books/details/:id' element={<ShowBook />} />
+      <Route path='/books/edit/:id' element={<EditBook />} />
+      <Route path='/books/delete/:id' element={<DeleteBook />} />
+    </Routers>
+  );
+};
 
 export default App
