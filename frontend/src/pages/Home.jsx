875c100462e44322c673d56b50bc2765e1eaaff0
Joshua Ye <joshua.ye2005@gmail.com>
2025-05-27 23:00:13 -0700

    frontend Home.jsx file

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,27 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import Spinner from '../components/Spinner.jsx';
-import { Link } from 'react-router-dom';
-import { AiOutlineEdit } from 'react-icons/ai';
-import { BsInfoCircle } from 'react-icons/bs';
-import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
+import React from 'react'
 
 const Home = () => {
-  const [loading, setLoading] = useState(false);
-  const [books, setBooks] = useState([]);
-
-  useEffect(() => {
-    setLoading(true);
-    axios.get('http://localhost:5555/books')
-    .then((response) => {
-        setBooks(response.data.data);
-        setLoading(false);
-    })
-    .catch((error) => { 
-        console.log(error);
-        setLoading(false);  
-    })    
-  })
   return (
     <div>Home</div>
   )
