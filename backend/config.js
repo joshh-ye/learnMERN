f5b927e3c0b1dfa7702689d19280f1f43bfab729
Joshua Ye <joshua.ye2005@gmail.com>
2025-06-01 16:48:26 -0700

    port env variable for online render

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
-export const PORT = 5000;
+export const PORT= process.env.PORT || 5000;
 
 export const mongoDBURL = 
 'mongodb+srv://josh:Abcd1234!@learnmern.ackuosw.mongodb.net/?retryWrites=true&w=majority&appName=learnMERN'
